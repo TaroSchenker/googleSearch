1b7f967194f0f00a9e294e86fabf8b4ce44b83f1
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-06-23 13:43:04 +0100

    updated error message

--- server/data/errorArray.js
+++ server/data/errorArray.js
@@ -1,6 +1,6 @@
 const errorArray = [{
     //  "url": "./client/static/searchResults.html",
-    // "heading": "There has been an error.",
+    // "heading": "Sorry, there has been a misunderstanding! This is a search engine for dog adoption which only allows searches for 'dog adoption' Please retry with the preferred search term",
     "bodyText": "Sorry, there has been a misunderstanding! This is a search engine for dog adoption which only allows searches for 'dog adoption' Please retry with the preferred search term",
 }]
 
