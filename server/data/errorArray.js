1d6b8f5974214d7ff3f75ff4a0ffd38202de799b
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-06-23 13:39:51 +0100

    updated error msg

--- server/data/errorArray.js
+++ server/data/errorArray.js
@@ -1,7 +1,7 @@
 const errorArray = [{
-    // "url": "",
+    //  "url": "./client/static/searchResults.html",
     // "heading": "There has been an error.",
-    "bodyText": "You have searched up a term that is not in the dataset. Please only search for dog adoption",
+    "bodyText": "Sorry, there has been a misunderstanding! This is a search engine for dog adoption which only allows searches for 'dog adoption' Please retry with the preferred search term",
 }]
 
 module.exports = errorArray;
